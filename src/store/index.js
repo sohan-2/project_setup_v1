baf0dc9df7f591b1bf6696c6e450ab2ed02f82ea
sohan-2 <sohanagale28@gmail.com>
2024-06-27 09:17:49 +0530

    redux clean up

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,10 @@
 // src/store.js
 import { configureStore } from "@reduxjs/toolkit";
-import { createSlice } from "@reduxjs/toolkit";
 import exampleReducer from "./exampleSlice";
-
-// Example slice
-// const exampleSlice = createSlice({
-//   name: "example",
-//   initialState: {},
-//   reducers: {
-//     // Define your reducers here
-//   },
-// });
-
-// export const { actions } = exampleSlice;
 
 const store = configureStore({
   reducer: {
     example: exampleReducer,
-    // example: exampleSlice.reducer,
   },
 });
 
